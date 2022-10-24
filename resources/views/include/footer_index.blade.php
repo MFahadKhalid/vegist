497a218c78f707de74f1bb32320afd136c11eaec
abdulwahabkhalid <98513650+MFahadKhalid@users.noreply.github.com>
2022-10-25 12:46:05 +1400

    vegist

---
+++ resources/views/include/footer_index.blade.php
@@ -0,0 +1,123 @@
+<section class="footer3 section0-tb-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="home3-footer">
+                            <div class="f-logo">
+                                <ul class="footer-ul">
+                                    <li class="footer-li footer-logo">
+                                        <a href="index3.html">
+                                            <img class="img-fluid" src="image/logo2.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li class="footer-li footer-address">
+                                        <ul class="foote-map">
+                                            <li class="footer-icon">
+                                                <i class="ion-ios-location"></i>
+                                            </li>
+                                            <li>
+                                                <span>401 Broadway 24th floor
+</span>
+                                                <span>Near ant mall cross road</span>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="footer-li footer-contact">
+                                        <ul class="footer-num">
+                                            <li class="footer-icon">
+                                                <i class="ion-ios-telephone"></i>
+                                            </li>
+                                            <li class="footer-info">
+                                                <a href="tel:1-800-222-000">Phone: 1-800-222-000</a>
+                                                <a href="mailto:demo@demo.com">Email: demo@demo.com</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="footer-bottom">
+                                <div class="footer-link" id="footer-accordian">
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Services</h2>
+                                        <a href="#t-cate" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Services</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="t-cate" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="about-us.html">About vegist</a></li>
+                                            <li class="f-link-ul-li"><a href="faq's.html">Faq's</a></li>
+                                            <li class="f-link-ul-li"><a href="contact.html">Contact us</a></li>
+                                            <li class="f-link-ul-li"><a href="blog-style-3-grid.html">News</a></li>
+                                            <li class="f-link-ul-li"><a href="sitemap.html">Sitemap</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Privacy & aerms</h2>
+                                        <a href="#services" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Privacy & terms</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="services" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="payment-policy.html">Payment policy</a></li>
+                                            <li class="f-link-ul-li"><a href="privacy-policy.html">Privacy policy</a></li>
+                                            <li class="f-link-ul-li"><a href="return-policy.html">Return policy</a></li>
+                                            <li class="f-link-ul-li"><a href="shipping-policy.html">Shipping policy</a></li>
+                                            <li class="f-link-ul-li"><a href="terms-conditions.html">Terms & conditions</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">My account</h2>
+                                        <a href="#privacy" data-bs-toggle="collapse" class="h-footer">
+                                            <span>My account</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="privacy" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="account.html">My account</a></li>
+                                            <li class="f-link-ul-li"><a href="cart-3.html">My cart</a></li>
+                                            <li class="f-link-ul-li"><a href="tracking.html">Order history</a></li>
+                                            <li class="f-link-ul-li"><a href="wishlist.html">My wishlist</a></li>
+                                            <li class="f-link-ul-li"><a href="addresses.html">My address</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="f-link">
+                                        <h2 class="h-footer">Menu title</h2>
+                                        <a href="#account" data-bs-toggle="collapse" class="h-footer">
+                                            <span>Menu title</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="f-link-ul collapse" id="account" data-bs-parent="#footer-accordian">
+                                            <li class="f-link-ul-li"><a href="product-style-3.html">Fruits</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-3.html">Fast foods</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-3.html">Vegetable</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-3.html">Salads</a></li>
+                                            <li class="f-link-ul-li"><a href="product-style-3.html">Bestseller</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="footer-deal">
+                                <div class="f-deal-content">
+                                    <h2>Get the latest deals</h2>
+                                    <p>Ana receive $20 coupon for first shopping</p>
+                                </div>
+                                <div class="footer-search">
+                                    <form>
+                                        <input type="text" name="email" placeholder="Enter your emain address">
+                                        <a href="javascript:void(0)" class="btn btn-style1"><i class="ion-paper-airplane"></i></a>
+                                    </form>
+                                    <ul class="f-bottom">
+                                        <li class="f-social">
+                                            <a href="https://www.whatsapp.com/" class="f-icn-link"><i class="fa fa-whatsapp"></i></a>
+                                            <a href="https://www.facebook.com/" class="f-icn-link"><i class="fa fa-facebook-f"></i></a>
+                                            <a href="https://twitter.com/" class="f-icn-link"><i class="fa fa-twitter"></i></a>
+                                            <a href="https://www.instagram.com/" class="f-icn-link"><i class="fa fa-instagram"></i></a>
+                                            <a href="https://www.pinterest.com/" class="f-icn-link"><i class="fa fa-pinterest-p"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,31 @@
+<script src="{{asset('js/modernizr-2.8.3.min.js')}}"></script>
+        <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+        <!-- bootstrap -->
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+        <!-- popper -->
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <!-- fontawesome -->
+        <script src="{{asset('js/fontawesome.min.js')}}"></script>
+        <!-- owl carousal -->
+        <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+        <!-- swiper -->
+        <script src="{{asset('js/swiper.min.js')}}"></script>
+        <!-- custom -->
+        <script src="{{asset('js/custom.js')}}"></script>
+
+        <!-- back to top end -->
+        <div class="mm-fullscreen-bg"></div>
+        <!-- jquery -->
+        <script src="js/modernizr-2.8.3.min.js"></script>
+        <!-- jquery -->
+        <script src="js/jquery-3.6.0.min.js"></script>
+        <!-- bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- popper -->
+        <script src="js/popper.min.js"></script>
+        <!-- owl carousal -->
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/fontawesome.min.js"></script>
+        <script src="js/swiper.min.js"></script>
+        <!-- custom -->
+        <script src="js/custom.js"></script>
